acbb5699de2c004ec407de462209fa32edc07d9b
Ana Raballo <anitaraballo@gmail.com>
2018-03-16 10:50:33 -0400

    Update and delete on adoption works

--- src/app/components/adopt-details/adopt-details.component.ts
+++ src/app/components/adopt-details/adopt-details.component.ts
@@ -7,8 +7,7 @@ import { environment } from "../../../environments/environment";
 
 import "rxjs/add/operator/toPromise"
 import { Route } from '@angular/router/src/config';
-
-// import { FileUploader} from "ng2-file-upload";
+// import { last } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-adopt-details',
@@ -16,14 +15,13 @@ import { Route } from '@angular/router/src/config';
   styleUrls: ['./adopt-details.component.css']
 })
 export class AdoptDetailsComponent implements OnInit {
-  dog = Object;
-
-  public updatedDog: Object = {}
-  public dogDescription: String;
-
-  saveError = "";
+  dog = <any>{}
 
   baseUrl = environment.apiBase;
+  public updateDog: Object = {}
+  public dogDescription: String;
+  
+  saveError = "";
 
   constructor(
     private myAdoptionService: AdoptionService,
@@ -38,24 +36,61 @@ export class AdoptDetailsComponent implements OnInit {
     });
     this.myAuthService
     .checklogin()
-  //If success, we are logged in.
+    //If success, we are logged in.
     .then()
 
- // Even if you don't do anything on error, catch to avoid a console error.
+    // Even if you don't do anything on error, catch to avoid a console error.
     .catch (err => {
       console.log(err);
       this.myRouter.navigate(["/adoptions/id"]);
     });
-
   }
 
+  // Getting one dog and it's details
   getDogDetails(id) {
     this.myAdoptionService.getId(id)
-    .then( res => {
-        this.dog = res;
+    .then( theDogDetails => {
+        this.dog = theDogDetails;
         console.log("dog details: ", this.dog)
     })
-    .catch()
   }
 
-}
+  doUpdate(id, formData){
+    const formInfo = formData.form.controls;
+    console.log("=============== formData: ", formInfo.dogDescription);
+
+    this.dogDescription = formInfo.dogDescription.value;
+    this.sendUpdatesToApi(id)
+  }
+
+  sendUpdatesToApi(id){
+    this.updateDog = { dogDescription: this.dog.description };
+    console.log( "updates: ", this.updateDog)
+      this.myAdoptionService.updatedDog(id, this.updateDog)
+      .toPromise()
+      .then(() => {
+        this.myRouter.navigate(['/adoption'])
+      })
+      .catch()
+      } 
+
+  deleteDog() {
+    if(!confirm("Are you sure?")) {
+      return;
+    }
+    this.myAdoptionService
+    .deleteDog(this.dog._id)
+    .then(() => {
+      console.log("Success");
+      this.myRouter.navigate(["/adoption"]);
+    })
+    .catch(err => {
+      alert("Sorry! Something went wrong.");
+      console.log("Dog Delete Error");
+      console.log(err);
+    });
+  }
+  }
+
+
+
--- src/app/services/adoption.service.ts
+++ src/app/services/adoption.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { environment } from "../../environments/environment";
 import 'rxjs/add/operator/map';
+import { environment } from "../../environments/environment";
 
 
 @Injectable()
@@ -11,21 +11,34 @@ export class AdoptionService {
 
   getAllAdoptions(){
     return this.myHttp.get(`${environment.apiBase}/api/adoption`,
-  {withCredentials: false})
-  .map( res => res.json())
+        { withCredentials: false })
+        .map( res => res.json())
   }
   
   getId(id){
     return this.myHttp.get(`${environment.apiBase}/api/adoption/${id}`,
-        { withCredentials: false})
-        .toPromise()
-        .then( res => res.json() )
+        { withCredentials: false })
+    .toPromise()
+    .then( res => res.json() )
   }
 
   createNewAdoption(dogData){
     return this.myHttp
-    .post(`${environment.apiBase}/api/adoption`, dogData, { withCredentials: true })
+    .post(`${environment.apiBase}/api/adoption`, dogData, 
+        { withCredentials: true })
     .toPromise()
     .then( res => res.json())
+  }
+
+  updatedDog(id, updates){
+    return this.myHttp.put(`${environment.apiBase}/api/adoption/${id}`, updates, 
+        { withCredentials: true })
+    .map(res => res.json());
+  }
+
+  deleteDog(id) {
+    return this.myHttp.delete(`${environment.apiBase}/api/adoption/${id}`,
+        { withCredentials: true })
+    .toPromise()
   }
 }

d0f477eda816f279f9739a04061808ccd532be30
Ana Raballo <anitaraballo@gmail.com>
2018-03-19 17:29:08 -0400

    Google maps perfect

--- src/app/components/google-maps/google-maps.component.ts
+++ src/app/components/google-maps/google-maps.component.ts
@@ -13,39 +13,20 @@ baseUrl = environment.apiBase;
 lostListError: string;
 
 // Zoom level
-zoom: number = 10;
+zoom: number = 5;
 
 // Start Position
 lat: number = 42.825583;
 lng: number = -71.018029;
 
 // Values
-markerName: string;
+markerLocation: string;
 markerLat: string;
 markerLng: string;
 markerDraggable: string;
 
 //Markers
-markers: marker[] = [
-  {
-    name: "Company One",
-    lat: 42.825583,
-    lng: -71.018029,
-    draggable: true
-  },
-  {
-    name: "Company Two",
-    lat: 42.868164,
-    lng: -70.889071,
-    draggable: true
-  },
-  {
-    name: "Company Three",
-    lat: 42.858279,
-    lng: -70.930498,
-    draggable: false
-  }
-]
+markers: marker[] = []
 
   constructor(private myLostService: LostService) { }
 
@@ -59,7 +40,7 @@ markers: marker[] = [
       allTheLostDogs.forEach(function(oneLostDog){
   
         var newMarker = {
-          name:"Untitled",
+          location: oneLostDog.location,
           lat: oneLostDog.latitude,
           lng: oneLostDog.longitude,
           draggable: false
@@ -73,38 +54,27 @@ markers: marker[] = [
   );
  
   };
-  // for each run a function that calls another function in your service
-  // the function in the service needs to make an api call to the express app and find all the dogs and returns a list
-  //in this component, receive the list, and loop through each dog and for each one do something like this:
-  //  var newMarker = {
-  //   name:"Untitled",
-  //   lat: dog.lat,
-  //   lng: dog.coords.lng,
-  //   draggable: false
-  // }
-  
-
 
   clickedMarker(marker: marker, index: number){
-    console.log('Clicked Marker: '+ marker.name + ' at index ' + index);
+    console.log('Clicked Marker: '+ marker.location + ' at index ' + index);
   }
 
-  mapClicked($event: any){
-    var newMarker = {
-      name:"Untitled",
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: false
-    }
+  // mapClicked($event: any){
+  //   var newMarker = {
+  //     name:"Untitled",
+  //     lat: $event.coords.lat,
+  //     lng: $event.coords.lng,
+  //     draggable: false
+  //   }
 
-    this.markers.push(newMarker);
-  }
+  //   this.markers.push(newMarker);
+  // }
 
   markerDragEnd(marker: any, $event: any){
     console.log('dragEnd', marker, $event);
 
     var updMarker = {
-      name: marker.name,
+      location: marker.location,
       lat: parseFloat(marker.lat),
       lng: parseFloat(marker.lng),
       draggable: false
@@ -123,20 +93,18 @@ markers: marker[] = [
     }
 
     var newMarker = {
-      name:this.markerName,
+      location:this.markerLocation,
       lat: parseFloat(this.markerLat),
       lng: parseFloat(this.markerLng),
       draggable: isDraggable
     }
     this.markers.push(newMarker);
   }
-
-
 }
 
 // Marker Type
 interface marker{
-  name?: string;
+  location?: string;
   lat: number;
   lng: number;
   draggable: boolean;
